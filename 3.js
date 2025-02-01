70559350d9217e51980c4e34f1e310fbe357beb4
Niki Kalwar <nikikumar27@gmail.com>
2025-02-01 06:38:16 +0530

    prototypes

---
+++ 3.js
@@ -0,0 +1,10 @@
+//prototypes examples
+function Bike(model,color){
+    this.model = model;
+    this.color = color;
+}
+
+Bike.prototype.getDetails = function() {
+    return this.model + 'bike has' + this.color
+}
+
